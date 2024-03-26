13db1e50ca01d1e98aa4763f3317160f4e193b9a
bagiscmp <gabriela.campos@estudante.ifms.edu.br>
2024-03-26 12:23:57 -0400

    primeira aula

---
+++ App.js
@@ -0,0 +1,20 @@
+//npm install -g npm@10.5.0
+
+import React from "react";
+import {View, Text} from "react-native";
+
+function App(){
+  return{
+
+    <View>
+    <Text> olá mundo! </Text>
+    </View>
+  };
+}
+
+//depois de criar, tem que colocar o export
+export default App; //poderia por antes do funcition
+
+
+import React, {Component} from "react";
+import {View, Text, Image} from "react-native";
